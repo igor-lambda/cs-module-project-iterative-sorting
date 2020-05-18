d8ecb49df6b6a3c1eed88bd3c9e6f6ea5d1c08bb
igor <embiggenerd@gmail.com>
2020-05-18 17:21:58 -0400

    binary search passing

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,29 @@
 def linear_search(arr, target):
     # Your code here
-
+    for index, item in enumerate(arr):
+        if item == target:
+            return index
 
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
     # Your code here
+    start = 0
+    end = len(arr) - 1
 
+    while start <= end:
+        mid = (end + start) // 2
 
-    return -1  # not found
+        if arr[mid] == target: # If mid is target, just return id
+            return mid
+
+        if arr[mid] < target:  # If el at mid is less than target, it means we have to look at the right side
+            start = mid + 1
+
+        elif arr[mid] > target:  # Else, look at the left
+            end = mid - 1
+
+    return -1 
+
